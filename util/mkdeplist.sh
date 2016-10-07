644a38779090475a8e8575d98c57d13f79a93b91
Jason Graham <jason.graham@jhuapl.edu>
2016-10-07 11:59:24 -0400

    Removed the METAPKG designation from PythonQt and paraview; updated util/mkdeplist.sh to deal with meta-packages.

--- util/mkdeplist.sh
+++ util/mkdeplist.sh
@@ -2,29 +2,41 @@
 
 DEPLIST=../deplist
 
-rm -f $DEPLIST
 ( 
     cd deps
 
+    rm -f $DEPLIST
+
     while read d
     do
-	echo $d
+	echo -n "$d"
 	echo -n "${d}: " >> $DEPLIST 
 	list=( `./sbopkg-dep2sqf -o -l $d` )
 
+	pkg=""
 	N=${#list[@]}
-	pkg=${list[$N-1]} # Package is at the end of the list
-	
-	if [[ $pkg != $d ]]; then
-	    echo "expected package name does not match the dep file name" 
-	    exit 1
+	if (( N == 0 )); then
+	    echo ": empty dep list for $d--possibly an empty METAPKG"
+	    echo >> $DEPLIST
+	    continue
+	else
+	    pkg=${list[$N-1]} # Package is at the end of the list
 	fi
 
+	# Checking if the expected package matches
+	if [[ $pkg != $d ]]; then
+	    echo -n ": expected package name does not match the dep file name--assuming a METAPKG" 
+	fi
+
+	echo -n ": "
 	for (( n=0; n<N-1; ++n ))
 	do
+	    echo -n "${list[$n]} "
 	    echo -n "${list[$n]} " >> $DEPLIST
 	done
 	echo >> $DEPLIST
+
+	echo
     done<PKGLIST
 )
 
