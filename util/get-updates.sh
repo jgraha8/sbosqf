059c4c18032d1cee849435c43c26511aa1c7b054
Jason Graham <jgraha8@gmail.com>
2017-08-31 00:20:54 -0400

    Fixed upgrade script

--- util/get-updates.sh
+++ util/get-updates.sh
@@ -7,7 +7,7 @@ UPDATES_FILE=sbopkg-updates
 
 trap 'rm -f $UPDATES_FILE $TMP_FILE ; exit 1' TERM INT
 
-sbopkg -c 2> /dev/null | grep ^[a-z] | grep : | cut -d: -f1 > $TMP_FILE
+sudo -i sbopkg -c 2> /dev/null | grep ^[a-z] | grep : | cut -d: -f1 > $TMP_FILE
 
 cat > $UPDATES_FILE<<EOF
 METAPKG
