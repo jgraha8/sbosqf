323767ff7299818224eae23e50dd0e011d6f0050
Jason Graham <jgraham@compukix.net>
2017-11-12 22:30:12 -0500

    Updated install-repo-sqf.sh script to place the files in repo top level SQF directory.

---
+++ util/install-repo-sqf.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -e
+
+source ~/.slack-repo
+DEP_DIR=~/slackware/sbopkg-dep2sqf/deps
+QUEUE_DIR=$REPO_DIR/SQF
+
+rm -rf $QUEUE_DIR
+mkdir -p $QUEUE_DIR
+
+cd $QUEUE_DIR
+
+while read p
+do
+    [ ! -d $REPO_DIR/$p ] && continue
+
+    sbopkg-dep2sqf -o $p
+    
+#    rm -f $REPO_DIR/$p/$p.sqf
+    #mv $p.sqf $QUEUE_DIR/$p.sqf
+
+done<${DEP_DIR}/PKGLIST
+
