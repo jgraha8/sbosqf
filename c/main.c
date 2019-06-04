24284ce50cd607501d2127e17100068717f3abe0
Jason Graham <jgraham@compukix.net>
2019-06-03 23:52:00 -0400

    Updated the main program options.

--- c/main.c
+++ c/main.c
@@ -30,27 +30,21 @@
                 long_opt, no_argument, 0, opt                                                                     \
         }
 
-static const char *options_str            = "CDNPRSacdehlmpru";
+static const char *options_str            = "ckunpRsehlrg";
 static const struct option long_options[] = {
     /* These options set a flag. */
-    LONG_OPT("check-installed", 'C'),
-    LONG_OPT("database", 'D'),
-    LONG_OPT("no-recursive", 'N'),
-    LONG_OPT("revdeps", 'P'),
-    LONG_OPT("review", 'R'),
-    LONG_OPT("search-pkg", 'S'),
-    LONG_OPT("add", 'a'),
-    LONG_OPT("check-foreign-installed", 'c'),
-    LONG_OPT("delete", 'd'),    
-    LONG_OPT("edit", 'e'),
-    LONG_OPT("help", 'h'),
-    LONG_OPT("list", 'l'),
-    LONG_OPT("menu", 'm'),
-/*    LONG_OPT("optional", 'o'), */
-    LONG_OPT("print", 'p'),
-    LONG_OPT("remove", 'r'),
-    LONG_OPT("search", 's'),
-    LONG_OPT("uninstall", 'u'),
+    LONG_OPT("check-installed", 'c'),   
+    LONG_OPT("check-foreign-installed", 'k'),    
+    LONG_OPT("update-pkgdb", 'u'), /* action */
+    LONG_OPT("no-recursive", 'n'),
+    LONG_OPT("revdeps", 'p'), 
+    LONG_OPT("review", 'R'),       /* action */
+    LONG_OPT("search", 's'),       /* action */
+    LONG_OPT("edit", 'e'),         /* action */
+    LONG_OPT("help", 'h'),         /* action */
+    LONG_OPT("list", 'l'),   
+    LONG_OPT("remove", 'r'),       /* action */
+    LONG_OPT("graph", 'g'), 
     {0, 0, 0, 0}};
 
 static const struct option *find_option(const char *long_name, const int name)
@@ -78,16 +72,11 @@ enum action {
         ACTION_NONE,
         ACTION_REVIEW,
         ACTION_ADD,
-        ACTION_CREATEDB,
-        ACTION_MENU,
-        ACTION_PRINT_TREE,
-	ACTION_DELETE_DEP,
+        ACTION_UPDATEDB,
         ACTION_EDIT_DEP,
-        ACTION_MANAGE_DEP,
 	ACTION_WRITE_SQF,
-	ACTION_REMOVE_PKG,
-	ACTION_SEARCH_PKG,
-	ACTION_SEARCH_DEP
+	ACTION_WRITE_REMOVE_SQF,
+	ACTION_SEARCH,
 };
 
 struct action_struct {
@@ -118,18 +107,17 @@ enum output_mode {
 
 int main(int argc, char **argv)
 {
-        struct process_options process_options;
         bool pkg_name_required = true;
 	enum output_mode output_mode = OUTPUT_FILE;
+	bool create_graph = false;
+
+	struct pkg_graph *pkg_graph = NULL;
+	struct pkg_options pkg_options = pkg_options_default();
 
         if (setvbuf(stdout, NULL, _IONBF, 0) != 0)
                 perror("setvbuf()");
 
-	memset(&process_options, 0, sizeof(process_options));
-	process_options.recursive = true;
-	process_options.optional = true;	
-
-        init_user_config();
+        user_config_init();
 
         struct action_struct as = {ACTION_NONE, NULL};
 
@@ -141,40 +129,31 @@ int main(int argc, char **argv)
                         break;
 
                 switch (c) {
-                case 'C':
-                        process_options.check_installed |= CHECK_INSTALLED;
+                case 'c':
+                        pkg_options.check_installed |= CHECK_INSTALLED;
                         break;
-                case 'D':
-                        set_action(&as, ACTION_CREATEDB, find_option(NULL, 'D'));
+                case 'k':
+                        pkg_options.check_installed |= CHECK_ANY_INSTALLED;
+                        break;
+                case 'u':
+                        set_action(&as, ACTION_UPDATEDB, find_option(NULL, 'u'));
                         pkg_name_required = false;
                         break;
-                case 'N':
+                case 'n':
                         process_options.recursive = false;
                         break;
-                case 'P':
-			if( as.action == ACTION_REMOVE_PKG ) {
-				fprintf(stderr, "option --revdeps/-P is ignored when using --remove/-r\n");
+                case 'p':
+			if( as.action == ACTION_WRITE_REMOVE_SQF ) {
+				fprintf(stderr, "option --revdeps is ignored when using --remove\n");
 				break;
 			}
-                        process_options.revdeps = true;
+                        pkg_options.revdeps = true;
                         break;
                 case 'R':
                         set_action(&as, ACTION_REVIEW, find_option(NULL, 'R'));
                         break;
-                case 'S':
-                        set_action(&as, ACTION_SEARCH_PKG, find_option(NULL, 'S'));
-                        break;
-                case 'm':
-                        set_action(&as, ACTION_MENU, find_option(NULL, 'm'));
-                        break;
-                case 'a':
-                        set_action(&as, ACTION_ADD, find_option(NULL, 'a'));
-                        break;
-                case 'c':
-                        process_options.check_installed |= CHECK_ANY_INSTALLED;
-                        break;
-                case 'd':
-                        set_action(&as, ACTION_DELETE_DEP, find_option(NULL, 'd'));
+                case 's':
+                        set_action(&as, ACTION_SEARCH, find_option(NULL, 's'));
                         break;
                 case 'e':
                         set_action(&as, ACTION_EDIT_DEP, find_option(NULL, 'e'));
@@ -183,24 +162,20 @@ int main(int argc, char **argv)
                         print_help();
                         exit(0);
                 case 'l':
-                        /* set_action(&as, ACTION_LIST, find_option(NULL, 'l')); */
 			output_mode = OUTPUT_STDOUT;
                         break;
-                /* case 'o': */
-                /*         process_options.optional = true; */
-                /*         break; */
-                case 'p':
-                        set_action(&as, ACTION_PRINT_TREE, find_option(NULL, 'p'));
+                case 'g':
+			create_graph = true;
                         break;
                 case 'r':
-			if( process_options.revdeps ) {
-				fprintf(stderr, "option --revdeps/-P is ignored when using --remove/-r\n");
+			if( pkg_options.revdeps ) {
+				fprintf(stderr, "option --revdeps is ignored when using --remove\n");
 				process_options.revdeps = false;
 			}
-                        set_action(&as, ACTION_REMOVE_PKG, find_option(NULL, 'r'));
+                        set_action(&as, ACTION_WRITE_REMOVE_SQF, find_option(NULL, 'r'));
                         break;
                 case 's':
-                        set_action(&as, ACTION_SEARCH_DEP, find_option(NULL, 's'));
+                        set_action(&as, ACTION_SEARCH, find_option(NULL, 's'));
                         break;
                 default:
                         abort();
@@ -216,7 +191,16 @@ int main(int argc, char **argv)
                 pkg_name = argv[optind];
         }
 
-        init_pkg_db();
+	pkg_graph = pkg_graph_alloc();
+	sbo_pkgs  = pkg_graph_sbo_pkgs(pkg_graph);
+
+        if (!pkg_db_exists()) {
+                pkg_load_sbo(sbo_pkgs);
+                pkg_write_db(sbo_pkgs);
+                pkg_create_default_deps(sbo_pkgs);
+        } else {
+                pkg_load_db(sbo_pkgs);
+        }
 
         int rc = 0;
 
@@ -224,40 +208,62 @@ int main(int argc, char **argv)
 		as.action = ACTION_WRITE_SQF;
 	}
 
+	struct pkg_node *pkg_node;
         switch (as.action) {
         case ACTION_REVIEW:
-		rc = perform_dep_action(pkg_name, MENU_REVIEW_PKG);
+		rc = pkg_load_dep(pkg_graph, pkg_name, pkg_options);
+		if( rc != 0 )
+			break;
+		
+		pkg_node = pkg_graph_search(pkg_graph, pkg_name);
+		if( pkg_node == NULL) {
+			fprintf(stderr, "package %s does not exist\n", pkg_name);
+			rc = 1;
+			break;
+		}		
+		rc = pkg_review(&pkg_node->pkg);
                 break;
-        case ACTION_ADD:
-                rc = perform_dep_action(pkg_name, MENU_ADD_PKG);
-                break;
-        case ACTION_CREATEDB:
-                if ((rc = write_depdb(process_options)) != 0)
-                        break;
-                rc = write_parentdb(process_options);
-                break;
+        case ACTION_UPDATEDB:
+                if( (rc = pkg_load_sbo(sbo_pkgs)) != 0 )
+			break;
+		if( (rc = pkg_write_db(sbo_pkgs)) != 0 )
+			break;
+		rc = pkg_create_default_deps(sbo_pkgs);
+		break;
         case ACTION_EDIT_DEP:
-                rc = perform_dep_action(pkg_name, MENU_EDIT_DEP);
+		fprintf(stderr, "option --edit is not yet implementated\n");
+		rc = 1;
                 break;
-        case ACTION_DELETE_DEP:
-                rc = perform_dep_action(pkg_name, MENU_DELETE_DEP);
-                break;
-        case ACTION_MENU:
-                if (find_dep_file(pkg_name) == NULL) {
-                        rc = display_dep_menu(pkg_name, msg_dep_file_not_found(pkg_name), 0);
-                } else {
-			rc = display_dep_menu(pkg_name, NULL, 0);
+	case ACTION_WRITE_SQF: {
+		rc = pkg_load_dep(pkg_graph, pkg_name, pkg_options);
+		if( rc != 0 )
+			break;
+		
+		char sqf[256];
+		if( pkg_options.revdeps ) {
+			bds_string_copyf(sqf, sizeof(sqf), "%s-revdeps.sqf", pkg_name);
+		} else {
+			bds_string_copyf(sqf, sizeof(sqf), "%s.sqf", pkg_name);
 		}
-                break;
-	case ACTION_WRITE_SQF:
-	case ACTION_REMOVE_PKG: {
-                if (find_dep_file(pkg_name) == NULL) {
-                        rc = display_dep_menu(pkg_name, msg_dep_file_not_found(pkg_name), 0);
-			if (find_dep_file(pkg_name) == NULL) {
-				rc = 1;
-				break;
-			}
-                }		
+		
+		FILE *fp = fopen(sqf, "w");
+		if( fp == NULL ) {
+			fprintf(stderr, "unable to create sqf file %s\n", sqf);
+			rc = 1;
+			break;
+		}
+		write_sqf(fp, pkg_graph, pkg_name, pkg_options);
+		fclose(fp);
+		break;
+	}		
+	case ACTION_WRITE_REMOVE_PKG: {
+		/*
+		  In case its a meta file, we go ahead and load it into the graph
+		 */
+		if( (rc = pkg_load_dep(pkg_graph, pkg_name, pkg_options)) != 0 )
+			break;
+		if( (rc = pkg_load_all_deps(pkg_graph, pkg_options)) != 0 )
+			break;
 
 		struct dep_list *dep_list = NULL;		
 		if( process_options.revdeps ) {
