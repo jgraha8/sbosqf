41ecaf16b1d7e9c8d364719865a58787f1afad08
Jason Graham <jgraham@ompukix.net>
2024-08-03 20:45:23 -0400

    Moved to sourcehut

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # sbosqf
 
 Dependency mangement and queue file generator for SlackBuilds.org repository
+
+## Moved to sourcehut
+
+This project is now moved to sourcehut https://git.sr.ht/~jgraha8/sbosqf.
--- util/download-jdk.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-url=$(sbopkg-dep2sqf -R jdk | grep DOWNLOAD_x86_64 | cut -d= -f2 | sed 's/\"//g')
-tgz=${url##*/}
-
-sudo wget -O /var/cache/sbopkg/$tgz \
-     -c --header "Cookie: oraclelicense=accept-securebackup-cookie" \
-     $url
-
